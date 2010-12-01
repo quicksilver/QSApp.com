2c689a7041cb5c910c3d2752ad9fbcd13b255304
Patrick Robertson <robertson.patrick@gmail.com>
2010-12-02 10:09:57 +1100

    Added a 'pull.php' page that will be used to pull content from Github

---
+++ pull.php
@@ -0,0 +1,5 @@
+<?php
+// This file will be used to pull content from the github repository.
+// Something like: type a list of files, hit enter, verify, then some PHP script will pull the files from 
+// https://github.com/quicksilver/QSApp.com/raw/master/whatever.file
+?>
