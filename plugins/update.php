1c4d139c79361b79fe6aeef807a5272728212bac
Etienne Samson <tiennou7@gmail.com>
2011-02-02 17:14:22 +0100

    Cleanup.

--- plugins/update.php
+++ plugins/update.php
@@ -2,12 +2,12 @@
 
 include('functions.php');
 
-class Plugins{
+class Plugins {
 	var $pluginsDir = 'plugins';
 	var $pluginsList;
 	var $pluginsCount = -1;
 
-	function getPluginsList(){
+	function getPluginsList() {
 
 		$this->pluginsList = array();
 
@@ -23,16 +23,16 @@ class Plugins{
 					}
 				}
 			}
-		}	
+		}
 
 		rsort($this->pluginsList);
 
-		return $this->pluginsList;
-
 		closedir($this->pluginsDir);
+
+		return $this->pluginsList;
 	}
 
-	function updatePlugins(){
+	function updatePlugins() {
 
 		connect_db();
 
@@ -42,39 +42,40 @@ class Plugins{
 		mysql_query("TRUNCATE TABLE plugins");
 
 		foreach (array_slice($list,0,999) as $fileNames) {
-			parseFilename($fileNames);
+			$this->parseFilename($fileNames);
 		}
 		close_db();
 		echo 'Success!';
 	}
+
+	function parseFilename($fileNames){
+
+		// Split the file name into the 3 parts - name, vers., mod date
+		$parts = explode("__", $fileNames);
+
+		$image = "images/".$parts[0].".png";
+
+		// replace the _ in the plugin name with a space
+		$plugin_name = str_replace("_", " ", $parts[0]);
+
+		// replace the _ in the version witha point
+		$vers = str_replace("_", ".", $parts[1]);
+
+		// remove the .zip from the mod date and replace _ with /
+		$mod  = substr($parts[2], 0 , -4);
+		$mod = str_replace("_", "-", $mod);	
+
+		$full_path = "plugins/".$fileNames;
+
+		$sql = "INSERT INTO plugins (image, name, version, moddate, fullpath) VALUES ('$image', '$plugin_name', '$vers', '$mod', '$full_path' ) ";
+		if (!mysql_query($sql))
+		{
+			die('Error: ' . mysql_error());
+		}
+
+	}
 }
 $pluginsHandler = new Plugins();
-function parseFilename($fileNames){
-
-	// Split the file name into the 3 parts - name, vers., mod date
-	$parts = explode("__", $fileNames);
-
-	$image = "images/".$parts[0].".png";
-
-	// replace the _ in the plugin name with a space
-	$plugin_name = str_replace("_", " ", $parts[0]);
-
-	// replace the _ in the version witha point
-	$vers = str_replace("_", ".", $parts[1]);
-
-	// remove the .zip from the mod date and replace _ with /
-	$mod  = substr($parts[2], 0 , -4);
-	$mod = str_replace("_", "-", $mod);	
-
-	$full_path = "plugins/".$fileNames;
-
-	$sql = "INSERT INTO plugins (image, name, version, moddate, fullpath) VALUES ('$image', '$plugin_name', '$vers', '$mod', '$full_path' ) ";
-	if (!mysql_query($sql))
-	{
-		die('Error: ' . mysql_error());
-	}
-
-}
 ?>
 <html>
 <head>
