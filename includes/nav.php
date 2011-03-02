0d10f7406ce7a38a2562fa8827fa305e1c9f17d1
Patrick Robertson <robertson.patrick@gmail.com>
2011-03-02 12:49:51 +1100

    Renamed forums.php to support.php Makes more sense

--- includes/nav.php
+++ includes/nav.php
@@ -3,4 +3,4 @@
 <a href="/plugins"><div class="link">Plugins</div></a>
 <a href="/wiki"><div class="link">Wiki</div></a>
 <a href="/download.php"><div class="link">Download</div></a>
-<a href="/forums.php"><div class="link">Support</div></a>
+<a href="/support.php"><div class="link">Support</div></a>
