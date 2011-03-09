1bed3934dbe270174fbf8d0500cf65a1f30accad
Patrick Robertson <robertson.patrick@gmail.com>
2011-03-09 23:30:21 +1100

    missed the http off a link

--- support.php
+++ support.php
@@ -35,7 +35,7 @@
 				<h3>Quicksilver Manual</h3>
 				<p>An extensive and well laid-out manual has been written by Howard Melman, which can be downloaded from <a href="http://blacktree-quicksilver.googlegroups.com/web/Quicksilver.pdf?_done=%252Fgroup%252Fblacktree-quicksilver%253F">here</a>.</p>
 				<h3>LoveQuicksilver Twitter &amp; Blog</h3>
-				<p>A good place for fixes and tips is the <a href="twitter.com/lovequicksilver">@LoveQuicksilver</a> twitter page by Philip Dooher.<br />
+				<p>A good place for fixes and tips is the <a href="http://twitter.com/lovequicksilver">@LoveQuicksilver</a> twitter page by Philip Dooher.<br />
 					Philip also runs a blog for Quicksilver tips at <a href="http://www.lovequicksilver.com/">LoveQuicksilver.com</a>. Here you can read existing tips or post your own.</p>
 					<h3>Bug Reporting</h3>
 				<p>To report a bug on Quicksilver, go to <a href="https://github.com/quicksilver/Quicksilver/issues" target="_blank">Quicksilver's GitHub repository</a>.<br />
