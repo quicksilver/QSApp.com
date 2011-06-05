a89d7b23c028d6779639d409713762257c534a9d
Patrick Robertson <robertson.patrick@gmail.com>
2011-06-05 18:14:00 +1000

    changes for ß60 update

--- changelog.php
+++ changelog.php
@@ -3,7 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-	<title>Quicksilver: Download</title>
+	<title>Quicksilver: Version Changelog</title>
 	<!-- Date: 2010-04-04 -->
 	<link href="default.css" rel="stylesheet" type="text/css"  />
 
@@ -30,16 +30,7 @@
 			<p style="color:#353535;font-size:20pt;margin-top:-10px;	font-variant: small-caps;">OS X at your fingertips</p>
 					      <?php include('includes/nav.php'); ?>
 			<div class="opener">
-				
-<h3>Download Quicksilver</h3>
-<p>To find out what's new in the latest version of Quicksilver, see the <a href="changelog.php">changelog page</a>.</p>
-			<h4>Mac OS X 10.6 Snow Leopard</h4>
-			<a class="dlbutton" href="http://cloud.github.com/downloads/quicksilver/Quicksilver/Quicksilver%20B60.dmg">Download &szlig;60</a>
-			<h4>Mac OS X 10.5 Leopard, 10.4 Tiger</h4>
-			<a class="dlbutton" href="https://github.com/downloads/quicksilver/Quicksilver/Quicksilver-B54.tgz">Download &szlig;54</a>
-			<h4>Mac OS X 10.3 Panther</h4>
-			<a class="dlbutton" href="https://github.com/downloads/quicksilver/Quicksilver/Quicksilver%20B36.tgz">Download &szlig;36</a>
-			<p>For a full explanation on the best Quicksilver version for your operating system, see the Wiki: <a href="http://qsapp.com/wiki/Which_version">Which Version?</a></p>
+<?php include('Changes.html'); ?>
 
 			</div>
 			<?php include('includes/footer.php'); ?>
