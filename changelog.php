b7e630edfa085ec191aa4058e9613e33bc75fa54
Dídac García <didaquis@gmail.com>
2020-12-23 00:01:17 +0100

    HTML has been cleaned

--- changelog.php
+++ changelog.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!DOCTYPE html>
+<html lang="en">
 	<head>
-  		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-		<meta http-equiv="Content-Language" content="en" />
-		<meta content="" name="description" />
-		<meta name="Keywords" content="" />
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Changelog of Quicksilver Mac OS X Utility for macOS" />
+		<meta name="keywords" content="Quicksilver, changelog, mac, macOS, os x, launcher, application, utility, productivity, itunes" />
 		<title>Quicksilver &#151; Changelog</title>
 		<?php include('template/head.tpl'); ?>
 	</head>
@@ -14,15 +13,15 @@
 			<?php include('template/logo.tpl'); ?>
 			<?php include('template/nav.tpl'); ?>
 			<div id="Page-Top"></div>
-			<div id="Page">
+			<main id="Page">
 				<h1>Quicksilver Changelog</h1>
-					<div class="Page-Break"></div>
-					<?php 
-						// This only works on production
-						@include('ChangesBare.html');
-					?>
-					<div class="Page-Break"></div>
-			</div>
+				<div class="Page-Break"></div>
+				<?php 
+					// This only works on production
+					@include('ChangesBare.html');
+				?>
+				<div class="Page-Break"></div>
+			</main>
 			<div id="Page-Bottom"></div>
 			<?php include('template/footer.tpl'); ?>
 		</div>
--- download.php
+++ download.php
@@ -1,47 +1,46 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <meta http-equiv="Content-Language" content="en" />
-  <meta content="Description" name="Download Quicksilver Mac OS X Utility for macOS" />
-   <meta name="Keywords" content="Quicksilver, download, mac, macos, os x, launcher, application, utility, productivity, itunes" />
-  <title>Quicksilver &#151; Download</title>
-  <?php include('template/head.tpl'); ?>
-</head>
-<body>
-  <div id="Container">
-    <?php include('template/logo.tpl'); ?>
-    <?php include('template/nav.tpl'); ?>
-    <div id="Page-Top"></div>
-    <div id="Page">
-      <h1>Download Quicksilver</h1>
-      <div class="Page-Break"></div>
-<p>To download Quicksilver for Mac OS X, select the compatible version for your operating system version.<br />
-	View the <a href="changelog.php">changelog</a> to see the recent changes to Quicksilver.</p>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Download Quicksilver Mac OS X Utility for macOS" />
+		<meta name="keywords" content="Quicksilver, download, mac, macOS, os x, launcher, application, utility, productivity, itunes" />
+		<title>Quicksilver &#151; Download</title>
+		<?php include('template/head.tpl'); ?>
+	</head>
+	<body>
+		<div id="Container">
+			<?php include('template/logo.tpl'); ?>
+			<?php include('template/nav.tpl'); ?>
+			<div id="Page-Top"></div>
+			<main id="Page">
+				<h1>Download Quicksilver</h1>
+				<div class="Page-Break"></div>
+				<p>To download Quicksilver for macOS, select the compatible version for your operating system version.<br />
+				View the <a href="changelog.php">changelog</a> to see the recent changes to Quicksilver.</p>
 
-      <div id="Download">
-        <div class="Download-Option">
-          <img src="images/quicksilver_512.png" width="256" height="256" alt="download Quicksilver" /><br />
-          <h4>The latest release for macOS 10.12 or later</h4>
-          <div class="Download-Button-Container">
-            <a class="Download-Button" href="https://qs0.qsapp.com/plugins/download.php">Download 1.6.1</a>
-          </div>
-        </div> <!-- Download Option -->
-        
-    <div class="Download-Option">
-          <img src="images/other.png" alt="Other Quicksilver Downloads" /><br />
-          <h4>All Downloads<br/><br/></h4>
-          <div class="Download-Button-Container">
-            <a class="Download-Button" href="/archives/">Download Older Versions</a>
-          </div>
-        </div> <!-- Download Option -->
+				<section id="Download">
+					<article class="Download-Option">
+						<img src="images/quicksilver_512.png" width="128" height="128" alt="Download latest release of Quicksilver" /><br />
+						<h2 class="Download-Title">The latest release for macOS 10.12 or later</h2>
+						<div class="Download-Button-Container">
+							<a class="Download-Button" href="https://qs0.qsapp.com/plugins/download.php">Download 1.6.1</a>
+						</div>
+					</article>
 
-        <div class="clear"></div>
-      </div> <!-- Download -->
+					<article class="Download-Option">
+						<img src="images/other.png" width="128" height="128" alt="Other Quicksilver Downloads" /><br />
+						<h2 class="Download-Title">All Downloads</h2>
+						<div class="Download-Button-Container">
+							<a class="Download-Button" href="/archives/">Download Older Versions</a>
+						</div>
+					</article>
 
-    </div> <!-- Page -->
-    <div id="Page-Bottom"></div>
-    <?php include('template/footer.tpl'); ?>
-  </div> <!-- Container -->
-</body>
+					<div class="clear"></div>
+				</section>
+			</main>
+			<div id="Page-Bottom"></div>
+			<?php include('template/footer.tpl'); ?>
+		</div>
+	</body>
 </html>
