ddb6e504190c63490321e3319c39d3fba8f94581
Patrick Robertson <robertson.patrick@gmail.com>
2022-03-28 23:27:47 +0800

    Add a donation confirm page for when people have donated

---
+++ donate-confirm.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Donate to the Quicksivler macOS project to help ensure the best Mac productivity application remains free forever." />
+		<meta name="keywords" content="quicksilver, mac, os x, macOS, productivity, application, launcher, itunes, paypal, donate, flattr, open source" />
+		<title>Quicksilver &#151; Donate</title>
+		<script type="text/javascript" src="/js/stumper.js"></script>
+		<script type="text/javascript">
+		/* <![CDATA[ */
+			(function() {
+				var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+				s.type = 'text/javascript';
+				s.async = true;
+				s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+				t.parentNode.insertBefore(s, t);
+			})();
+		/* ]]> */
+		</script>
+		<!-- For the donate button (from Flattr) Please email us as devs [at] qsapp [dot] com if this breaches terms -->
+		<style type="text/css">
+			.resp-sharing-button__link,
+			.resp-sharing-button__icon {
+			  display: inline-block
+			}
+
+			.resp-sharing-button__link {
+			  text-decoration: none;
+			  color: #fff;
+			  margin: 0.5em
+			}
+
+			.resp-sharing-button {
+			  border-radius: 5px;
+			  transition: 25ms ease-out;
+			  padding: 0.5em 0.75em;
+			  font-family: Helvetica Neue,Helvetica,Arial,sans-serif;
+				text-shadow:none;
+				font-weight:normal;
+			}
+
+			.resp-sharing-button__icon svg {
+			  width: 1em;
+			  height: 1em;
+			  margin-right: 0.4em;
+			  vertical-align: top
+			}
+
+			.resp-sharing-button--small svg {
+			  margin: 0 0 2px 0;
+			  vertical-align: middle
+			}
+
+			/* Non solid icons get a stroke */
+			.resp-sharing-button__icon {
+			  stroke: #fff;
+			  fill: none
+			}
+
+			/* Solid icons get a fill */
+			.resp-sharing-button__icon--solid,
+			.resp-sharing-button__icon--solidcircle {
+			  fill: #fff;
+			  stroke: none
+			}
+
+			.resp-sharing-button--twitter {
+			  background-color: #55acee
+			}
+
+			.resp-sharing-button--twitter:hover {
+			  background-color: #2795e9
+			}
+
+			.resp-sharing-button--pinterest {
+			  background-color: #bd081c
+			}
+
+			.resp-sharing-button--pinterest:hover {
+			  background-color: #8c0615
+			}
+
+			.resp-sharing-button--facebook {
+			  background-color: #3b5998
+			}
+
+			.resp-sharing-button--facebook:hover {
+			  background-color: #2d4373
+			}
+
+			.resp-sharing-button--tumblr {
+			  background-color: #35465C
+			}
+
+			.resp-sharing-button--tumblr:hover {
+			  background-color: #222d3c
+			}
+
+			.resp-sharing-button--reddit {
+			  background-color: #5f99cf
+			}
+
+			.resp-sharing-button--reddit:hover {
+			  background-color: #3a80c1
+			}
+
+			.resp-sharing-button--google {
+			  background-color: #dd4b39
+			}
+
+			.resp-sharing-button--google:hover {
+			  background-color: #c23321
+			}
+
+			.resp-sharing-button--linkedin {
+			  background-color: #0077b5
+			}
+
+			.resp-sharing-button--linkedin:hover {
+			  background-color: #046293
+			}
+
+			.resp-sharing-button--email {
+			  background-color: #777
+			}
+
+			.resp-sharing-button--email:hover {
+			  background-color: #5e5e5e
+			}
+
+			.resp-sharing-button--xing {
+			  background-color: #1a7576
+			}
+
+			.resp-sharing-button--xing:hover {
+			  background-color: #114c4c
+			}
+
+			.resp-sharing-button--whatsapp {
+			  background-color: #25D366
+			}
+
+			.resp-sharing-button--whatsapp:hover {
+			  background-color: #1da851
+			}
+
+			.resp-sharing-button--hackernews {
+			background-color: #FF6600
+			}
+			.resp-sharing-button--hackernews:hover, .resp-sharing-button--hackernews:focus {   background-color: #FB6200 }
+
+			.resp-sharing-button--vk {
+			  background-color: #507299
+			}
+
+			.resp-sharing-button--vk:hover {
+			  background-color: #43648c
+			}
+
+			.resp-sharing-button--facebook {
+			  background-color: #3b5998;
+			  border-color: #3b5998;
+			}
+
+			.resp-sharing-button--facebook:hover,
+			.resp-sharing-button--facebook:active {
+			  background-color: #2d4373;
+			  border-color: #2d4373;
+			}
+
+			.resp-sharing-button--twitter {
+			  background-color: #55acee;
+			  border-color: #55acee;
+			}
+
+			.resp-sharing-button--twitter:hover,
+			.resp-sharing-button--twitter:active {
+			  background-color: #2795e9;
+			  border-color: #2795e9;
+			}
+
+			.resp-sharing-button--tumblr {
+			  background-color: #35465C;
+			  border-color: #35465C;
+			}
+
+			.resp-sharing-button--tumblr:hover,
+			.resp-sharing-button--tumblr:active {
+			  background-color: #222d3c;
+			  border-color: #222d3c;
+			}
+
+			.resp-sharing-button--email {
+			  background-color: #777777;
+			  border-color: #777777;
+			}
+
+			.resp-sharing-button--email:hover,
+			.resp-sharing-button--email:active {
+			  background-color: #5e5e5e;
+			  border-color: #5e5e5e;
+			}
+
+			.resp-sharing-button--whatsapp {
+			  background-color: #25D366;
+			  border-color: #25D366;
+			}
+
+			.resp-sharing-button--whatsapp:hover,
+			.resp-sharing-button--whatsapp:active {
+			  background-color: #1DA851;
+			  border-color: #1DA851;
+			}
+		</style>
+		<?php include('template/head.tpl'); ?>
+	</head>
+	<body>
+		<div id="Container">
+			<?php include('template/logo.tpl'); ?>
+			<?php include('template/nav.tpl'); ?>	
+			<div id="Page-Top"></div>
+			<main id="Page">
+				<h1>Thank You for Supporting Quicksilver!</h1>
+				<div class="Page-Break"></div>
+				<p>Your donation will help us fund future development of Quicksilver. This will cover things like funding development licenses, server costs, and reimbursing volunteer team costs.</p>
+<p>&nbsp;</p>
+					<h2>Other ways to Help</h2>
+					<p>Looking for other ways to help support Quicksilver's growth? View our <a href="/volunteer-projects.php">list of projects</a> you can get involved in.</p>
+<p>&nbsp;</p>
+					<h3>Share this Page with Others</h3>
+
+<!-- Sharingbutton Facebook -->
+<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=https%3A%2F%2Fqsapp.com%2Fdonate.php" target="_blank" rel="noopener" aria-label="">
+  <div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z"/></svg>
+    </div>
+  </div>
+</a>
+
+<!-- Sharingbutton Twitter -->
+<a class="resp-sharing-button__link" href="https://twitter.com/intent/tweet/?text=I%20just%20donated%20to%20Quicksilver%2C%20the%20macOS%20productivity%20app%20that%20will%20change%20the%20way%20you%20use%20your%20Mac!&amp;url=https%3A%2F%2Fqsapp.com%2Fdonate.php" target="_blank" rel="noopener" aria-label="">
+  <div class="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm5.26 9.38v.34c0 3.48-2.64 7.5-7.48 7.5-1.48 0-2.87-.44-4.03-1.2 1.37.17 2.77-.2 3.9-1.08-1.16-.02-2.13-.78-2.46-1.83.38.1.8.07 1.17-.03-1.2-.24-2.1-1.3-2.1-2.58v-.05c.35.2.75.32 1.18.33-.7-.47-1.17-1.28-1.17-2.2 0-.47.13-.92.36-1.3C7.94 8.85 9.88 9.9 12.06 10c-.04-.2-.06-.4-.06-.6 0-1.46 1.18-2.63 2.63-2.63.76 0 1.44.3 1.92.82.6-.12 1.95-.27 1.95-.27-.35.53-.72 1.66-1.24 2.04z"/></svg>
+    </div>
+  </div>
+</a>
+
+<!-- Sharingbutton Tumblr -->
+<a class="resp-sharing-button__link" href="https://www.tumblr.com/widgets/share/tool?posttype=link&amp;title=I%20just%20donated%20to%20Quicksilver%2C%20the%20macOS%20productivity%20app%20that%20will%20change%20the%20way%20you%20use%20your%20Mac!&amp;caption=I%20just%20donated%20to%20Quicksilver%2C%20the%20macOS%20productivity%20app%20that%20will%20change%20the%20way%20you%20use%20your%20Mac!&amp;content=https%3A%2F%2Fqsapp.com%2Fdonate.php&amp;canonicalUrl=https%3A%2F%2Fqsapp.com%2Fdonate.php&amp;shareSource=tumblr_share_button" target="_blank" rel="noopener" aria-label="">
+  <div class="resp-sharing-button resp-sharing-button--tumblr resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <svg version="1.1" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
+        <path d="M12,0C5.383,0,0,5.383,0,12s5.383,12,12,12s12-5.383,12-12S18.617,0,12,0z M15.492,17.616C11.401,19.544,9.5,17,9.5,14.031 V9.5h-2V8.142c0.549-0.178,1.236-0.435,1.627-0.768c0.393-0.334,0.707-0.733,0.943-1.2c0.238-0.467,0.401-0.954,0.49-1.675H12.5v3h2 v2h-2v3.719c0,2.468,1.484,2.692,2.992,1.701V17.616z"/>
+     </svg>
+    </div>
+  </div>
+</a>
+
+<!-- Sharingbutton E-Mail -->
+<a class="resp-sharing-button__link" href="mailto:?subject=I%20just%20donated%20to%20Quicksilver%2C%20the%20macOS%20productivity%20app%20that%20will%20change%20the%20way%20you%20use%20your%20Mac!&amp;body=https%3A%2F%2Fqsapp.com%2Fdonate.php" target="_self" rel="noopener" aria-label="">
+  <div class="resp-sharing-button resp-sharing-button--email resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm8 16c0 1.1-.9 2-2 2H6c-1.1 0-2-.9-2-2V8c0-1.1.9-2 2-2h12c1.1 0 2 .9 2 2v8z"/><path d="M17.9 8.18c-.2-.2-.5-.24-.72-.07L12 12.38 6.82 8.1c-.22-.16-.53-.13-.7.08s-.15.53.06.7l3.62 2.97-3.57 2.23c-.23.14-.3.45-.15.7.1.14.25.22.42.22.1 0 .18-.02.27-.08l3.85-2.4 1.06.87c.1.04.2.1.32.1s.23-.06.32-.1l1.06-.9 3.86 2.4c.08.06.17.1.26.1.17 0 .33-.1.42-.25.15-.24.08-.55-.15-.7l-3.57-2.22 3.62-2.96c.2-.2.24-.5.07-.72z"/></svg>
+    </div>
+  </div>
+</a>
+
+<!-- Sharingbutton WhatsApp -->
+<a class="resp-sharing-button__link" href="whatsapp://send?text=I%20just%20donated%20to%20Quicksilver%2C%20the%20macOS%20productivity%20app%20that%20will%20change%20the%20way%20you%20use%20your%20Mac!%20https%3A%2F%2Fqsapp.com%2Fdonate.php" target="_blank" rel="noopener" aria-label="">
+  <div class="resp-sharing-button resp-sharing-button--whatsapp resp-sharing-button--small"><div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" viewBox="0 0 24 24"><path d="m12 0c-6.6 0-12 5.4-12 12s5.4 12 12 12 12-5.4 12-12-5.4-12-12-12zm0 3.8c2.2 0 4.2 0.9 5.7 2.4 1.6 1.5 2.4 3.6 2.5 5.7 0 4.5-3.6 8.1-8.1 8.1-1.4 0-2.7-0.4-3.9-1l-4.4 1.1 1.2-4.2c-0.8-1.2-1.1-2.6-1.1-4 0-4.5 3.6-8.1 8.1-8.1zm0.1 1.5c-3.7 0-6.7 3-6.7 6.7 0 1.3 0.3 2.5 1 3.6l0.1 0.3-0.7 2.4 2.5-0.7 0.3 0.099c1 0.7 2.2 1 3.4 1 3.7 0 6.8-3 6.9-6.6 0-1.8-0.7-3.5-2-4.8s-3-2-4.8-2zm-3 2.9h0.4c0.2 0 0.4-0.099 0.5 0.3s0.5 1.5 0.6 1.7 0.1 0.2 0 0.3-0.1 0.2-0.2 0.3l-0.3 0.3c-0.1 0.1-0.2 0.2-0.1 0.4 0.2 0.2 0.6 0.9 1.2 1.4 0.7 0.7 1.4 0.9 1.6 1 0.2 0 0.3 0.001 0.4-0.099s0.5-0.6 0.6-0.8c0.2-0.2 0.3-0.2 0.5-0.1l1.4 0.7c0.2 0.1 0.3 0.2 0.5 0.3 0 0.1 0.1 0.5-0.099 1s-1 0.9-1.4 1c-0.3 0-0.8 0.001-1.3-0.099-0.3-0.1-0.7-0.2-1.2-0.4-2.1-0.9-3.4-3-3.5-3.1s-0.8-1.1-0.8-2.1c0-1 0.5-1.5 0.7-1.7s0.4-0.3 0.5-0.3z"/></svg>
+    </div>
+  </div>
+</a>
+			</main>
+			<div id="Page-Bottom"></div>
+			<?php include('template/footer.tpl'); ?>
+		</div>
+	</body>
+</html>
