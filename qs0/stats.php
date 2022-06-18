b00b6f323752a4869b237615294291abd6ccf381
Patrick Robertson <robertson.patrick@gmail.com>
2022-06-18 03:44:57 +0000

    Get stats working of sorts

--- qs0/stats.php
+++ qs0/stats.php
@@ -2,16 +2,12 @@
 
 include('../lib/functions.php');
 
-$sql = "SELECT DISTINCT 'version' FROM" . STATS_TABLE;
+$sql = "SELECT DISTINCT version FROM " . STATS_TABLE;
 $versions = fetch_db($sql);
-
-$i = 1;
 foreach ($versions as $version) {
-	if ($i = 1) {}
-	$sql = "SELECT 'DATE', 'version' FROM" . STATS_TABLE . "WHERE version = \"" . $versions ."ORDER BY " . STATS_TABLE . ".`date` ASC";
-	$versionstats = fetch_db($qsl);
-	$array = $versionstats->
+	$sql = "SELECT date, version, downloads FROM " . STATS_TABLE . " WHERE version = \"" . $version['version'] ."\" ORDER BY " . STATS_TABLE . ".`date` ASC;";
+	$versionstats = fetch_db($sql);
+	foreach ($versionstats as $v){
+		echo $v['date'].",".$v['version'].",".$v['downloads']."\n";
 	}
 }
-
-?>
