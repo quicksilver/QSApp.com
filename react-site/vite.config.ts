40d4fbaf6de911f0d8278808bd35c86a58bdc5c9
Patrick Robertson <robertson.patrick@gmail.com>
2026-02-06 17:44:53 +0000

    Remove rollup plugin visualiser

--- react-site/vite.config.ts
+++ react-site/vite.config.ts
@@ -2,18 +2,13 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { visualizer } from "rollup-plugin-visualizer";
+
 const APP_BASE_PATH = "/"
 
 // https://vite.dev/config/
 export default defineConfig({
   base: APP_BASE_PATH,
-  plugins: [react(), tailwindcss(), visualizer({
-      filename: './dist/stats.html',
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-    })],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
